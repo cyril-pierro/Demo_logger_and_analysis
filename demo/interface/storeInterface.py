15fdebbee053b93adb4a9bfe3972b405c9c58af8
Peter Badasu <pbadasu@quantumgroupgh.com>
2022-03-30 15:09:16 +0000

    modify store interface codebase

--- demo/interface/storeInterface.py
+++ demo/interface/storeInterface.py
@@ -2,14 +2,22 @@ from abc import ABC, abstractmethod
 
 
 class StoreI(ABC):
-    """Store Interface"""
+    """
+    Store Interface class
+    """
 
     @abstractmethod
     def sell(self, product_name: str, product_value: int):
-        """Sell Product"""
+        """
+        Sell Product method
+        """
 
     def buy(self, product_name: str, product_value: int):
-        """Buy Product"""
+        """
+        Buy Product method
+        """
 
     def add(self, product_name: str, product_value: int):
-        """Add Product to Store"""
+        """
+        Add Product  method
+        """
